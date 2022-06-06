07fd487713037cdb08a9673552bb3c0ba190e573
War Buggy <hovanbuu@gmail.com>
2022-06-06 14:55:13 +0700

    _ fix corrent api web part

--- payment/payment.js
+++ payment/payment.js
@@ -6,7 +6,7 @@ const db = require('../db/db.js');
 module.exports = function (app) {
     //#region /api/payment/make
     //a request payment is made
-    app.post('/api/payment/make', function (request, response) {
+    app.post('/payment/make', function (request, response) {
         let requestIp = common.getReadableIP(request);
         let purpose = 'making a payment';
         common.consoleLog(`(${requestIp}) Received request for ${purpose}.`);

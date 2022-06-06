02ac476c2df21437526b7cbe097d50804a555c53
War Buggy <hovanbuu@gmail.com>
2022-06-06 15:40:01 +0700

    _ add basic param checking for make payment api

--- payment/payment.js
+++ payment/payment.js
@@ -2,6 +2,7 @@ const paymentConfig = require('./config.js');
 const common = require('../common.js');
 const systemConfig = require('../systemConfig.js');
 const db = require('../db/db.js');
+const { isNumeric } = require('../common.js');
 
 module.exports = function (app) {
     //#region /api/payment/make
@@ -15,16 +16,43 @@ module.exports = function (app) {
         let email = (request.body.email || '').trim();
         let deliveryAddress = (request.body.deliveryAddress || '').trim();
         let note = (request.body.note || '').trim();
+        let total = request.body.note || '';
 
         console.log(`cartString: ${cartString}`);
         console.log(`email: ${email}`);
         console.log(`deliveryAddress: ${deliveryAddress}`);
         console.log(`note: ${note}`);
+        console.log(`total: ${total}`);
 
         if (!common.validateEmail(email)) {
             let errorCode = 600;
-            common.consoleLogError(`Error when ${purpose}: Email is not valid (${email})`);
+            common.consoleLogError(`Error when ${purpose}: Email is not valid (${email}).`);
             response.status(errorCode);
+            response.json({ success: false, });
+            return;
+        }
+
+        if (deliveryAddress == '') {
+            let errorCode = 601;
+            common.consoleLogError(`Error when ${purpose}: Missing delivery address.`);
+            response.status(errorCode);
+            response.json({ success: false, });
+            return;
+        }
+
+        if (!common.isNumeric(total)) {
+            let errorCode = 602;
+            common.consoleLogError(`Error when ${purpose}: Total is not a number (${total}).`);
+            response.status(errorCode);
+            response.json({ success: false, });
+            return;
+        }
+
+        let checkCartStringResult = checkCartString(cartString);
+        if (!checkCartStringResult.result) {
+            let errorCode = 603;
+            common.consoleLogError(`Error when ${purpose}: ${checkCartStringResult.errorMessage}.`);
+            response.status(errorCode + checkCartStringResult.errorCode);
             response.json({ success: false, });
             return;
         }
@@ -37,4 +65,61 @@ module.exports = function (app) {
         response.json(resJson);
         common.consoleLog(`(${requestIp}) Request for ${purpose} was successfully handled.`);
     });
+
+    function checkCartString(cartString) {
+        if (cartString == '') {
+            return {
+                result: false,
+                errorCode: 0,
+                errorMessage: 'Empty cart',
+            };
+        }
+
+        let itemStringList = cartString.spit('|||');
+        let productIDList = [];
+        let itemList = [];
+        for (let i = 0; i < itemStringList.length; i++) {
+            let itemString = itemStringList[i];
+            let itemInfo = itemString.split(',');
+            if (itemInfo.length != 3) {
+                return {
+                    result: false,
+                    errorCode: 1,
+                    errorMessage: `Bad item info string (${itemInfo})`,
+                };
+            }
+            let productId = itemInfo[0];
+            let quantity = itemInfo[1];
+            let price = itemInfo[2];
+            if (!common.isNumeric(productId)) {
+                return {
+                    result: false,
+                    errorCode: 2,
+                    errorMessage: `Product ID is NaN (${itemInfo})`,
+                };
+            }
+            if (!common.isNumeric(quantity)) {
+                return {
+                    result: false,
+                    errorCode: 3,
+                    errorMessage: `Purchase quantity is NaN (${itemInfo})`,
+                };
+            }
+            if (!common.isNumeric(price)) {
+                return {
+                    result: false,
+                    errorCode: 4,
+                    errorMessage: `Price is NaN (${itemInfo})`,
+                };
+            }
+            productIDList.push(productId);
+            let item = {
+                productId,
+                quantity,
+                price,
+            };
+            itemList.push(item);
+        }
+        return { result: true, itemList, };
+    };
 };

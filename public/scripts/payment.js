58ac163dbd704904497e72c985438c590ae12ef3
War Buggy <hovanbuu@gmail.com>
2022-06-08 15:42:04 +0700

    _ change test payment cartString

--- public/scripts/payment.js
+++ public/scripts/payment.js
@@ -4,7 +4,7 @@ window.createOrder = async function () {
         deliveryAddress: '233 Acbfkfe St, ACBDEF, EKLMN, VIM',
         note: 'Xin giao hàng vào lúc tối. Gọi số 0xxx xxx xxx để nhận hàng',
         total: 300,
-        cartString: '2;2;60;0|||2;3;60;0',
+        cartString: '4;1;4;0',
     };
     try {
         let response = await Common.sendToBackend('payment/make', sendData);

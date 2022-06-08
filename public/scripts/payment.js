65e348ef6690ebff62360a0e81a24f571719d817
War Buggy <hovanbuu@gmail.com>
2022-06-08 14:51:07 +0700

    _ test redirection

---
+++ public/scripts/payment.js
@@ -0,0 +1,15 @@
+window.createOrder = async function () {
+    let sendData = {
+        email: 'tester@zacbdefz.com',
+        deliveryAddress: '233 Acbfkfe St, ACBDEF, EKLMN, VIM',
+        note: 'Xin giao hàng vào lúc tối. Gọi số 0xxx xxx xxx để nhận hàng',
+        total: 300,
+        cartString: '2;2;60;0|||2;3;60;0',
+    };
+    try {
+        let response = await Common.sendToBackend('/payment/make', sendData);
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+};
